c74b34bc5e3e8f9cbbe85533f52b73de6dea0c81
peter <fr33wayz@gmail.com>
2018-01-07 13:51:41 +0100

    change callback.js from callback hell to promise style

---
+++ javascript/callbacks.js
@@ -0,0 +1,27 @@
+const books = require('./books')
+
+const print = result => console.log('Result:', result)
+
+const getBooksAsync = (books) => {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+}
+const sortBooksAlphabetically = (books) => {
+    let sortedBooks = books
+    /* sorting logic books */
+    return getBooksAsync(sortedBooks);
+}
+
+const filterBooksWithTitleStartingWithA = (books) => {
+    let filteredBooks = books
+    /* filter logic here */
+    return getBooksAsync(books);
+}
+
+getBooksAsync(books)
+    .then(sortBooksAlphabetically)
+    .then(filterBooksWithTitleStartingWithA)
+    .then(print)
+
+// why
